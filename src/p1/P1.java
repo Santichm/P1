b5023e6a116e6bd06d381bac0ace802519797a44
Santiago Chaparro Martin <santiagochaparromartin@localhost>
2021-11-04 15:32:38 +0100

    Segundo commit, versión 2.

--- src/p1/P1.java
+++ src/p1/P1.java
@@ -15,7 +15,7 @@ public class P1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Versión 2.");
     }
     
 }
